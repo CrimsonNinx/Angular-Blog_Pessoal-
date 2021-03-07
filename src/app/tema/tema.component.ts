06866d9f4472387683b9fb050f857644a7d51580
Erick Costa Ferreira <74927000+CrimsonNinx@users.noreply.github.com>
2021-03-07 00:58:34 -0300

    adm

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
+import { AlertasService } from '../service/alertas.service';
 import { TemaService } from '../service/tema.service';
 
 @Component({
@@ -17,13 +18,19 @@ export class TemaComponent implements OnInit {
 
   constructor(
     private router :Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alertas:AlertasService
   ) { }
  
   ngOnInit(){
     if(environment.token == ''){
       
       this.router.navigate(['entrar'])
+    }
+
+    if(environment.tipo != 'adm'){
+      this.alertas.showAlertInfo('Voce precisa ser adm para acesar essa rota') 
+      this.router.navigate(['/inicio'])
     }
 this.findAllTemas(
 
@@ -38,7 +45,7 @@ findAllTemas(){
 
   cadastrar() {
     this.temaService.postTema(this.tema).subscribe((resp: Tema) => { this.tema = resp
-    alert('Tema cadastrado com sucesso')
+    this.alertas.showAlertSucess('Tema cadastrado com sucesso')
     this.findAllTemas()
  
     this.tema = new Tema() 
